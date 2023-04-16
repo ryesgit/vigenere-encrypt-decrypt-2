43bbb8f8629cf02ceac2cf564737dd2ff262f466
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-04-17 04:58:26 +0800

    Lower down user input for encryption

--- vigenere_encrypt_decrypt.py
+++ vigenere_encrypt_decrypt.py
@@ -20,3 +20,9 @@ NUM_KEYS = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f',
                11: 'l', 12: 'm', 13: 'n', 14: 'o', 15: 'p', 
                16: 'q', 17: 'r', 18: 's', 19: 't', 20: 'u', 
                21: 'v', 22: 'w', 23: 'x', 24: 'y', 25: 'z'}
+
+def encrypt(text, key):
+    resultant_num = []
+
+    text = text.lower()
+    key = key.lower()
